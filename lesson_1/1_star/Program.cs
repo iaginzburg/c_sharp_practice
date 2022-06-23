e1f71c6eca3418aabae2ce6bed51b0405c6d8f4f
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 12:47:25 +0300

    added task 1_star

---
+++ lesson_1/1_star/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите целое число");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 0) { 
+Console.Write("Модуль ввеленного числа ");
+Console.WriteLine(-num);
+}
+else {
+   Console.Write("Модуль введенного числа ");
+Console.WriteLine(num); 
+}
