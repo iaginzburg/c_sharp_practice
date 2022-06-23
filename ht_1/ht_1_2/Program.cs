d7d442796b4a29fbd94b04ad7ebdda6e3fe75fe0
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 14:47:12 +0300

    add hometask 2

---
+++ ht_1/ht_1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+double num3 = double.Parse(Console.ReadLine());
+
+if (num1 > num2) { 
+    if (num1 > num3){
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(num1);
+    } 
+    else {
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(num3);
+    }
+}
+else {
+    if (num2 > num3){
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(num2);
+    } 
+    else {
+    Console.WriteLine("Наибольшее число");
+    Console.WriteLine(num3);
+    }
+}
