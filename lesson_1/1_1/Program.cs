c1cdceba472e8f553e1fc2e694bb1c93e292b38f
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 11:14:28 +0300

    Add task 1

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World 1!");
+﻿/*
+string num_str = Console.ReadLine();
+int num_int = int.Parse(num_str);
+Console.WriteLine(num_int*num_int);
+*/
+
+/*
+int num_int = int.Parse(Console.ReadLine());
+Console.WriteLine(num_int*num_int);
+*/
+Console.WriteLine("Введите первое число");
+int num_int_1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num_int_2 = int.Parse(Console.ReadLine());
+
+if (num_int_1*num_int_1 == num_int_2){
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else{
+    Console.WriteLine("Второе число НЕ является квадратом второго");
+}
