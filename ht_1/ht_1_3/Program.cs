979f9145281818b941a860d4277e470a5bb65885
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 15:07:16 +0300

    add hometask 3

---
+++ ht_1/ht_1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program3
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число");
+        double num = double.Parse(Console.ReadLine());
+
+        if (num % 1 != 0)
+        {
+            Console.WriteLine("введено не целое число");
+        }
+        else if (num%2 == 0){
+                Console.WriteLine("это четное число");
+                } else {
+                Console.WriteLine("это нечетное число");
+                }
+
+    }
+}
