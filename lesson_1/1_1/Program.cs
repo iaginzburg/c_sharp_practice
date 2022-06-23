332724def01b2ad56d37c1ebead48ac6b5643786
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 13:11:49 +0300

    add folder for hometasks to lesson1 and add some changes to 1_1 from lesson

--- lesson_1/1_1/Program.cs
+++ lesson_1/1_1/Program.cs
@@ -1,13 +1,20 @@
-﻿/*
+﻿//Это задача номер 0, на вывод квадрата введенного числа
+//Развернутое решение
+/*
 string num_str = Console.ReadLine();
 int num_int = int.Parse(num_str);
 Console.WriteLine(num_int*num_int);
 */
-
+//Оптимизированное по коду решение
 /*
 int num_int = int.Parse(Console.ReadLine());
 Console.WriteLine(num_int*num_int);
 */
+
+
+
+//Это задача номер 1
+
 Console.WriteLine("Введите первое число");
 int num_int_1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
