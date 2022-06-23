40485cc23b8115aa86d831c905dd627f9f529bd0
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 15:33:54 +0300

    add hometask 4

---
+++ ht_1/ht_1_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+double num = double.Parse(Console.ReadLine());
+
+if (num > 0) {     
+double evenNum = 2;
+while (evenNum <= num){
+    Console.WriteLine(evenNum);
+    evenNum += 2;
+    }
+} 
+else {
+double evenNum = 0;
+while (evenNum >= num){
+    Console.WriteLine(evenNum);
+    evenNum -= 2;
+    }
+} 
+          
