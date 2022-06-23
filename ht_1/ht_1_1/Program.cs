57371b0d47ecd22db35d949047e29a339626b21a
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-23 13:36:16 +0300

    add hometask 1

--- ht_1/ht_1_1/Program.cs
+++ ht_1/ht_1_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+double num2 = double.Parse(Console.ReadLine());
+
+if (num1 >= num2) { 
+if (num1 == num2){
+    Console.WriteLine("Числа равны");
+    } 
+    else {
+       Console.WriteLine("Первое число больше второго");
+    }
+}
+else {
+   Console.WriteLine("Первое число меньше второго");
+}
