56a50b564442646075a251e0fd72f18f6b70b6b0
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-06-27 11:31:08 +0300

    add tsks 2,3,4

---
+++ lesson_2/2_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//newRandom().Next(10, 99)
+
+int MaxDigitOfNum(int num)
+{
+
+if (num % 10 > num / 10)
+    {
+        return num % 10;
+    }
+    else
+    {
+        return num / 10;
+    }
+
+}
+
+int num1 = new Random().Next(10, 100);
+Console.WriteLine (num1);
+Console.WriteLine(MaxDigitOfNum (num1));
+// Console.WriteLine(MaxDigitOfNum (newRandom().Next(10, 100)));
