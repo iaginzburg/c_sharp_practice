7ebd430068104ec29cdf3110df7640a77dc867cd
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-07-12 22:43:23 +0300

    added all tasks of lesson 6th and hometasks

---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void DecToBinString (int number){
+
+    string binary = "";
+    while (number > 0){
+        binary = number %2 + binary;
+        number /=2;
+    }
+   Console.WriteLine(binary);
+}
+
+DecToBinString(2);
+DecToBinString(45);
+
+// void DecToEightString (int number){
+
+//     string eight = "";
+//     while (number > 0){
+//         eight = number %8 + eight;
+//         number /=8;
+//     }
+//    Console.WriteLine(eight);
+// }
+
+// DecToEightString(79);
+// DecToEightString(17);
---
+++ lesson_6/6_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+// int Fibonacci (int n){
+// if (n == 1) return 1;
+// if (n == 2) return 1;
+// return Fibonacci(n-1) + Fibonacci (n-2);
+// }
+
+// for (int i = 1; i < 10; i++){
+// Console.WriteLine($"{i} - {Fibonacci(i)}");
+// }
+
+
+void Fibonacci (int number){
+    int num1 = 0;
+    int num2 = 1;
+    
+    for (int i = 0; i < number; i++){
+        Console.Write ($"{num1} ");
+        (num1, num2) = (num2, num1+num2);
+    }
+
+}
+
+Fibonacci(7);
