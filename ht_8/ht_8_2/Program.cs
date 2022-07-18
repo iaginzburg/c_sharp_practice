65d97e57de16ddce8aff06ddae8d59e4b0b7e9fe
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-07-18 14:45:34 +0300

    added all task from lesson 8, and all hometasks after lesson 8

--- ht_8/ht_8_2/Program.cs
+++ ht_8/ht_8_2/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+
+
+//Функция печати массива, вспомогательная
+void PrintIntArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindMinSumLine (int [,] array)
+{
+    int [] sumOfLines = new int [array.GetLength(0)];       //создаем и заполняем массив сумм строк
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sumOfLines[i] += array[i, j];
+        }
+    }
+
+    int indexMinSumLine = 0;                                 
+                                                            //наименьший элемент массива (его идекс = индексу искомой строки)
+    for (int i = 0; i < array.GetLength(0); i++){
+        if (sumOfLines[indexMinSumLine] > sumOfLines[i]) indexMinSumLine = i;
+    }
+
+    Console.WriteLine($"строка с наименьшей суммой элементов {indexMinSumLine+1}"); //вывод порядкового номера искомой строки
+}
+
+int[,] array = new int[4, 4] { { 100, 4, 7, 2 }, { 55, 9, 2, 3 }, { 8, 4, 2, 4 }, { 2, 25, 5, 2 } };
+PrintIntArray(array);
+Console.WriteLine();
+FindMinSumLine(array);
---
+++ lesson_8/8_help_Print_and_Fill_Func/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintDoubleArray(double [,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] FillArrayFunc (int m, int n)
+{
+    double [,] resultArray = new double [m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            resultArray[i, j] = Math.Round(new Random().NextDouble() * (10+10)-10, 1); 
+        }
+    }
+
+    return resultArray;
+}
+
+PrintDoubleArray(FillArrayFunc(3,4));
