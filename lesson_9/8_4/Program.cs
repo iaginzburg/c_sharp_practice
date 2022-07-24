194671923afe160a61e5bb5d6ae761c2df0b5a79
Ilya Ginzburg <iaginzburg88@gmail.com>
2022-07-24 21:27:43 +0300

    added all tasks from lesson 9 and hometasks after lesson 9

---
+++ lesson_9/8_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+
+
+int GradeOfNum (int num, int grade){
+    if (grade == 0 )  return 1;
+    return GradeOfNum(num, grade-1)  *num;
+}
+
+Console.WriteLine(GradeOfNum(3, 5));
+Console.WriteLine(GradeOfNum(2, 3));
+Console.WriteLine(GradeOfNum(2, 0));
